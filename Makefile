ec191c22d92808c6e1d4c6d0309cac7e30d2309b
Rounak Singh <rounaksingh17@gmail.com>
2012-10-06 19:35:49 +0530

    Makefile Add

---
+++ Makefile
@@ -0,0 +1,17 @@
+
+#
+#
+#
+#
+
+GCC = gcc
+
+SOURCE = pngimage
+SOURCE_EXT = cc
+
+all   :   compile
+
+compile : 
+	$(GCC) $(SOURCE).$(SOURCE_EXT) -o $(SOURCE) -I/usr/local/include\
+	 -L/usr/local/lib -lpng -lpngwriter -lz -DNO_FREETYPE
+
--- bmp2hex.c
+++ bmp2hex.c
@@ -2,16 +2,16 @@
 #include "file_error.h"
 /*
 	-BMP_READ to read the bmp file & write into hex.
-	-BMP_WRITE to read the text file from microController
-			& write into bmp.
+
 */
 #define FILE_OUT
-#define BUFFER_SIZE 4096		//don't make too large for small files.
-//#define BMP_WRITE 1
+#define BUFFER_SIZE 4096
+#define MAX_LINE 0x10
 
 //function prototype
-//unsigned int print_buf(char *buf_ptr,unsigned int buf_size);
 unsigned int print_buf(char *buf_ptr,unsigned int buf_size,long int *count_ptr);
+int hex_write(FILE *file_write,int *counter);
+int start_of_file(FILE *file_write);
 
 int main()
 {
@@ -21,20 +21,20 @@ int main()
 	#endif
 
 	unsigned char c[BUFFER_SIZE];
-	long int i=0; 		//if BUFFER_SIZE is long make it long.
+	long int i=0;
 	long int counter=0;
 ////////////////////////////////////////////////////////////////////////////
-	//#if defined BMP_READ
+
 	file_stream_in=fopen("Cover.bmp","rb");
 	if(file_stream_in==NULL)
 	return FILE_OPEN_ERROR;
 
 	#ifdef FILE_OUT
-	file_stream_out=fopen("Cover_test.bmp","wb");
+	file_stream_out=fopen("Cover_test.txt","wb");
 	if(file_stream_out==NULL)
 	return FILE_OPEN_ERROR;
-	#endif	
-	
+	#endif
+
 	/*
 	fseek(file_stream_in, 0, SEEK_END);
 	i = ftell(file_stream_in);
@@ -47,6 +47,7 @@ int main()
 	automatically.
 	try any bmp file,,,first check the size of file in bytes.
 	*/
+	/*
 	while(!feof(file_stream_in))
 	{
 	i=fread(&c[0],sizeof(unsigned char),BUFFER_SIZE,file_stream_in);
@@ -62,12 +63,12 @@ int main()
 		fseek(file_stream_in,i,SEEK_SET);
 		if(i==ftell(file_stream_in));
 		printf("%ld ",i);
-	
+
 	}
 */
 	/* TO print each byte with its offset number
-	*/
 	
+
 		if(i!=BUFFER_SIZE)
 
 		#ifdef FILE_OUT
@@ -90,6 +91,8 @@ int main()
 		#endif	
 	
 	}
+	*/
+	start_of_file(file_stream_out);
 
 	fclose(file_stream_in);
 	#ifdef FILE_OUT
@@ -113,3 +116,22 @@ unsigned int print_buf(char *buf_ptr,unsigned int buf_size,long int *count_ptr)
 return (buf_size);
 
 }
+
+int hex_write(FILE *file_write,int *counter)
+{
+	return 9;
+}
+
+int start_of_file(FILE *file_write)
+{
+	char c;
+	int i;
+	for(i=1;i<MAX_LINE;i++)
+	{
+		c=i;
+		fprintf(file_write,"%2X\t",c);
+	}
+	fprintf(file_write,"\n");
+	return 0;
+}
+
